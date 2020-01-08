05c1a614d40fad5f93f946ec2dc7b970f3e4c7b3
Edoardo Canobbio <canobbio.e@gmail.com>
2020-01-08 17:05:03 +0100

    Implemented webservice interface

---
+++ interfaces/webservice.go
@@ -0,0 +1,91 @@
+package interfaces
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/CanobbioE/web-service-example/domain"
+)
+
+// AdoptionInteractor define the usecases behavior.
+// Using an interface here allows to change implementation
+// as needed, for example mocking it during testing.
+type AdoptionInteractor interface {
+	Adopt(userID, animalID int) error
+	AdoptedAnimals(userID int) ([]domain.Animal, error)
+	AdoptableAnimals() ([]domain.Animal, error)
+}
+
+// WebserviceHandler represents the mechanism that transform
+// HTTP requests to data that the usecases layer can comprehend.
+type WebserviceHandler struct {
+	AdoptionInteractor AdoptionInteractor
+}
+
+// AdoptAnimal receives a POST request to adopt an animal
+// on behalf of the requesting client.
+func (wh WebserviceHandler) AdoptAnimal(w http.ResponseWriter, r *http.Request) {
+	userID, err := strconv.Atoi(r.URL.Query().Get("user"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	animalID, err := strconv.Atoi(r.URL.Query().Get("animal"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := wh.AdoptionInteractor.Adopt(userID, animalID); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	return
+}
+
+// ShowAdoptions receives a GET request to list all the animals
+// adopted by the requesting user.
+func (wh WebserviceHandler) ShowAdoptions(w http.ResponseWriter, r *http.Request) {
+	userID, err := strconv.Atoi(r.URL.Query().Get("user"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	adoptions, err := wh.AdoptionInteractor.AdoptedAnimals(userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	ret, err := json.Marshal(adoptions)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(ret)
+
+	return
+}
+
+// ShowAnimals receives a GET request to list all the available animals,
+func (wh WebserviceHandler) ShowAnimals(w http.ResponseWriter, r *http.Request) {
+	animals, err := wh.AdoptionInteractor.AdoptableAnimals()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	ret, err := json.Marshal(animals)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(ret)
+}
